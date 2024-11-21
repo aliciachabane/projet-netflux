52b2e4db33f0265e2abd839c62d5ce5b7aab9289
Alicia Chabane <alicia.chabane@laplateforme.io>
2024-11-21 20:56:55 +0100

    modif,ajout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "../components/Header.css";
+import netflix from "../assets/NETFLIX.svg";
+import original from "../assets/ORIGINAL.svg";
+import narcos from "../assets/logo-narcos.svg";
+import description from "../assets/Description.svg";
+import play from "../assets/btn-play.svg";
+import list from "../assets/btn-my-list.svg";
+
 function Header() {
     return (
          
         <header className="head">
-           
+            <img className="netflix" src={netflix} alt="Netflix" /> 
+            <img className="original" src={original} alt="original" /> 
+            <div className="narcos">
+            <img className="logo-narcos" src={narcos} alt="Narcos-logo" />     
+            </div>
+            <img className="description" src={description} alt="description" />     
+            <div className="btn">
+            <img className="btn-play" src={play} alt="btn-play" />   
+            <img className="btn-list" src={list} alt="btn-list" />   
+            </div>
         </header>
-    );
+    
+
+);
 }
 
 export default Header;
